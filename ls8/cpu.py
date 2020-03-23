d0b110b3110fa9d045455919527721f853e31735
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2020-03-23 17:51:21 +0100

    MVP reached

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,12 +7,19 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.properties = [0] * 8
+        self.reg = [0] * 8
+        self.pc = 0
+        self.ir = [0] * 256
+        self.ram = {} 
+        self.can_run = False
+        
 
     def load(self):
         """Load a program into memory."""
 
         address = 0
+        self.can_run = True
 
         # For now, we've just hardcoded a program:
 
@@ -59,7 +66,53 @@ class CPU:
             print(" %02X" % self.reg[i], end='')
 
         print()
+    
+    def ram_read(self, address):
+        return self.ram[address]
+    
+    def ram_write(self,address, value):
+        self.ram[address] = value
+        return self.ram[address]
+        
+        
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        while self.can_run:
+            #get instruction from ram
+            ram_read_ins = self.ram_read(self.pc)
+            # result = self.ram_write(self.ir, ram_read_ins)
+            self.ir[self.pc] = ram_read_ins
+            if ram_read_ins == 0b00000001:
+                self.can_run = False
+            format_ram_read_ins = '{0:8b}'.format(ram_read_ins)
+            num_op = int(format_ram_read_ins[:2].strip() or '00',2)
+            alu_op = int(format_ram_read_ins[2].strip() or '0',2)
+            inst_set = int(format_ram_read_ins[3].strip() or '0',2)
+            inst_iden = int(format_ram_read_ins[4:].strip() or '0000',2)
+
+            if alu_op == int('1', 2):
+                # self.alu(op, reg_a, reg_b)
+                pass
+            else:
+                if ram_read_ins == 0b10000010:
+                    key = int(self.ram_read(self.pc+1))
+                    value = self.ram_read(self.pc+2)
+                    self.ir[key] = value
+                    self.pc += 1
+                elif ram_read_ins == 0b01000111:
+                    key = int(self.ram_read(self.pc+1))
+                    print(int(self.ir[key]))
+                    self.pc += 1
+            if inst_set is not int('1', 2):
+                self.pc += int(num_op)
+
+
+
+
+
+            #DECODE operation
+            
+            #EXECUTE op.
+
